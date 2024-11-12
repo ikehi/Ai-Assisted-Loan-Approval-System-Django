5fb7e7b2df4ed6ef1e8eebb6ac953f932c703ad7
Matthias E. Ikehi <matthiasikehi@gmail.com>
2024-11-13 00:21:49 +0100

    Update

--- loan_management/settings.py
+++ loan_management/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-3=y%gpdsio6%!(-7i*rnw(cb9y%y$n@8b+ugywtys59p5k&fhj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-service-name.onrender.com']
+ALLOWED_HOSTS = ['ai-assisted-loan-approval-system-django.onrender.com']
+
 
 
 
